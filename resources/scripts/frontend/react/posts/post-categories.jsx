78bed4fe30ce11bbcb430eab535d9e87c47ca3d6
Paulo Ferreira <paulo.mateus.f@gmail.com>
2020-07-31 17:47:18 -0300

    Added loader spinner and post categories list (static).

--- resources/scripts/frontend/react/posts/post-categories.jsx
+++ resources/scripts/frontend/react/posts/post-categories.jsx
@@ -1,9 +1,9 @@
 export default function PostCategories() {
   return (
     <nav className="posts__categories">
-      <a href="#lorem" className="posts__term">Todos</a>
-      <a href="#lorem" className="posts__term is-active">Lorem</a>
-      <a href="#lorem" className="posts__term">Ipsum</a>
+      <button type="button" className="posts__term">Todos</button>
+      <button type="button" className="posts__term is-active">Lorem</button>
+      <button type="button" className="posts__term">Ipsum</button>
     </nav>
   );
 }
